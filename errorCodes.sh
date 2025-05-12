81c9b7fe3919eec68b7fce46c9710f5e7ee6e77e
admin <admin@etaTest.local>
2025-05-12 12:40:13 -0400

    error check func added

--- errorCodes.sh
+++ errorCodes.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-if [[ $? -eq 0 ]]; then
-	echo -e "\n"
-else
-	echo -e "error at: " >&2
-fi
+
+function check_error {
+    echo -e "\nChecking "$2" for errors..."
+    if [ $1 -ne 0 ]; then
+        echo -e "\t"$2" failed with error code $1." >&2
+    else
+        echo -e "\tNo errors found in "$2"."
+    fi
+}
